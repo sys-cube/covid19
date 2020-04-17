4867e70ac4027e44f9e2cd518f7b71a3f549ff9a
tsudaharuka <h_tsuda@sys-cube.co.jp>
2020-04-17 17:42:18 +0900

    企業の皆様、はたらく皆様へ　リンク修正

--- pages/worker.vue
+++ pages/worker.vue
@@ -6,15 +6,15 @@
     <static-card>
       <h3>
         <external-link
-          url="http://www.sangyo-rodo.metro.tokyo.jp/chushou/kinyu/yuushi/yuushi/new"
+          url="https://www.pref.wakayama.lg.jp/prefg/060100/d00203270.html"
           :icon-size="24"
-          >{{ $t('新型コロナウイルス感染症に対応した制度融資') }}
+          >{{ $t('新型コロナウイルス発生に関する県内事業者への「相談窓口」の開設') }}
         </external-link>
       </h3>
       <p>
         {{
           $t(
-            '感染症により事業活動に影響を受けている中小企業等を対象とした制度融資（「緊急融資」、「緊急借換」、「危機対応融資」）です。いずれも融資限度額は2億8千万円で、信用保証料を都が補助します。'
+            '和歌山県商工観光労働部では、新型コロナウイルスの流行により、影響を受けるまたは、その恐れがある県内事業者の皆様を支援するため、下記の相談窓口を設置します。'
           )
         }}
       </p>
@@ -22,15 +22,15 @@
     <static-card>
       <h3>
         <external-link
-          url="https://www.tokyo-kosha.or.jp/topics/2003/0001.html"
+          url="https://www.wakayama-sangyo.com/2020/03/09/%e6%96%b0%e5%9e%8b%e3%82%b3%e3%83%ad%e3%83%8a%e3%82%a6%e3%82%a4%e3%83%ab%e3%82%b9%e3%81%ab%e9%96%a2%e3%81%99%e3%82%8b%e7%9c%8c%e5%86%85%e4%ba%8b%e6%a5%ad%e8%80%85%e5%90%91%e3%81%91%e6%94%af%e6%8f%b4/"
           :icon-size="24"
-          >{{ $t('新型コロナウイルスによる経営課題に関する専門家派遣') }}
+          >{{ $t('県内事業者向け支援策(外部リンク)') }}
         </external-link>
       </h3>
       <p>
         {{
           $t(
-            '新型コロナウイルス感染症により経営面の影響を受けている中小企業を対象に、中小企業診断士等の専門家を無料で派遣し、経営改善等に向けたアドバイスを実施します（1社あたり4回まで・無料）。'
+            '各団体による相談窓口、関連対策へのリンクが記載されています。'
           )
         }}
       </p>
@@ -38,15 +38,15 @@
     <static-card>
       <h3>
         <external-link
-          url="https://www.tokyo-kosha.or.jp/support/josei/jigyo/kinkyu.html"
+          url="https://www.pref.wakayama.lg.jp/prefg/060300/gyoumu/kinyuu/sangyoushien.html"
           :icon-size="24"
-          >{{ $t('緊急販路開拓助成事業（新型コロナウイルス感染症緊急対策）') }}
+          >{{ $t('中小企業者への金融支援について') }}
         </external-link>
       </h3>
       <p>
         {{
           $t(
-            '感染症の影響を受け、売上が減少した都内中小企業の展示会出展費用の一部を助成します（限度額：150万円 / 助成率：5分の4 / 助成対象期間：交付決定日から1年1カ月 / 受付期間：5月11日～20日〈予定〉）。'
+            '主な金融支援策について一覧表がありますので融資制度をご活用されたい企業様はご確認ください。経営の安定化や事業の活性化に必要な資金を円滑に調達していただくために、県と金融機関、信用保証協会が協力して行う融資制度を設けました。'
           )
         }}
       </p>
@@ -54,15 +54,15 @@
     <static-card>
       <h3>
         <external-link
-          url="https://www.hataraku.metro.tokyo.jp/kansensyo/index.html"
+          url="https://www.pref.wakayama.lg.jp/prefg/060600/d00203536.html"
           :icon-size="24"
-          >{{ $t('中小企業従業員融資') }}
+          >{{ $t('テレワーク緊急導入支援プログラムのご紹介') }}
         </external-link>
       </h3>
       <p>
         {{
           $t(
-            '感染症等の影響による休業での収入減等に備え、中小企業の従業員向けに実質無利子の融資を行います（融資限度額100万円）。'
+            '今般、日本テレワーク協会（以下、JTA）において、テレワークの緊急導入をお考えの企業向けに、JTA会員企業・団体によるテレワーク緊急導入支援プログラムがとりまとめられましたので、ご紹介いたします。'
           )
         }}
       </p>
@@ -70,17 +70,17 @@
     <static-card>
       <h3>
         <external-link
-          url="https://www.hataraku.metro.tokyo.jp/kansensyo/index.html"
+          url="https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/0000164708_00001.html#hatarakukata"
           :icon-size="24"
           >{{
-            $t('新型コロナウイルス感染症に係る休業等支援事業（専門家派遣）')
+            $t('働く方と経営者の皆様へ（厚生労働省ページ）（外部リンク）')
           }}
         </external-link>
       </h3>
       <p>
         {{
           $t(
-            '感染症の拡大を理由とする従業員の休業等にあたり、国の雇用調整助成金の特例措置等を利用する中小企業等を対象に、社会保険労務士派遣による相談助言を行います（1社5回まで。無料）。'
+            '厚生労働省の公式ホームページにて働く方・経営者への支援について、雇用調整助成金、テレワーク、小学校等の臨時休業等に関する支援、妊娠中の女性労働者等への配慮、関係団体への要請、融資・貸付等、社会保険料の猶予等についての詳細が記載されています。'
           )
         }}
       </p>
@@ -88,121 +88,15 @@
     <static-card>
       <h3>
         <external-link
-          url="https://www.hataraku.metro.tokyo.jp/kansensyo/index.html"
+          url="https://jsite.mhlw.go.jp/wakayama-roudoukyoku/newpage_00100.html"
           :icon-size="24"
-          >{{ $t('新型コロナウイルス感染症対策雇用環境整備促進事業') }}
+          >{{ $t('小学校等の臨時休業に伴う支援等相談窓口（厚生労働省和歌山労働局）（外部リンク）') }}
         </external-link>
       </h3>
       <p>
         {{
           $t(
-            '感染症の拡大を理由として、国の雇用調整助成金等を活用し、非常時における勤務体制づくりなど職場環境整備に取り組む都内中小企業に奨励金を支給します（1事業所10万円）。'
-          )
-        }}
-      </p>
-    </static-card>
-    <static-card>
-      <h3>
-        <external-link
-          url="https://www.hataraku.metro.tokyo.jp/kansensyo/index.html"
-          :icon-size="24"
-          >{{ $t('テレワーク導入モデル体験事業') }}
-        </external-link>
-      </h3>
-      <p>
-        {{
-          $t(
-            'テレワーク導入を検討している中堅・中小企業等を対象に、テレワークを体験できる端末やツールを無償貸与することにより、テレワークのメリット・効果を体感できる機会を提供します。'
-          )
-        }}
-      </p>
-    </static-card>
-    <static-card>
-      <h3>
-        <external-link
-          url="https://www.shigotozaidan.or.jp/koyo-kankyo/joseikin/kinkyutaisaku.html"
-          :icon-size="24"
-          >{{ $t('事業継続緊急対策（テレワーク）助成金') }}
-        </external-link>
-      </h3>
-      <p>
-        {{
-          $t(
-            '都内中堅・中小企業に対し、テレワークの導入に必要な機器やソフトウェア等の経費を助成します（限度額250万円、助成率10/10）。ぜひご活用ください。'
-          )
-        }}
-      </p>
-    </static-card>
-    <static-card>
-      <h3>
-        <external-link url="https://tokyo-telework.jp/" :icon-size="24"
-          >{{ $t('テレワークオンラインセミナー開催') }}
-        </external-link>
-      </h3>
-      <p>
-        {{
-          $t(
-            '経済団体と連携したテレワーク導入のきっかけづくりのためのオンラインセミナーを開催します。'
-          )
-        }}
-      </p>
-    </static-card>
-    <static-card>
-      <h3>
-        <external-link
-          url="http://www.sangyo-rodo.metro.tokyo.jp/attention/2020/0305_13201.html"
-          :icon-size="24"
-          >{{ $t('中小企業者等特別相談窓口') }}
-        </external-link>
-      </h3>
-      <p>
-        {{ $t('資金繰りに関する相談、経営に関する相談') }}
-      </p>
-    </static-card>
-    <static-card>
-      <h3>
-        <external-link
-          url="http://www.sangyo-rodo.metro.tokyo.jp/attention/2020/0305_13201.html"
-          :icon-size="24"
-          >{{ $t('フリーランスを含む個人事業主特別相談窓口') }}
-        </external-link>
-      </h3>
-      <p>
-        {{
-          $t('資金繰りに関する相談、経営に関する相談（契約におけるトラブル等）')
-        }}
-      </p>
-    </static-card>
-    <static-card>
-      <h3>
-        <external-link
-          url="https://www.hataraku.metro.tokyo.lg.jp/sodan/sodan/index.html"
-          :icon-size="24"
-          >{{ $t('緊急労働相談ダイヤル') }}
-        </external-link>
-      </h3>
-      <p>
-        {{
-          $t(
-            '新型コロナウイルスに関する休暇や休業の取り扱い、職場のハラスメント、内定取り消しなどについての相談'
-          )
-        }}
-      </p>
-    </static-card>
-    <static-card>
-      <h3>
-        <external-link
-          url="https://smooth-biz.metro.tokyo.lg.jp/"
-          :icon-size="24"
-          >{{
-            $t('新しいワークスタイルや企業活動の東京モデル「スムーズビズ」')
-          }}
-        </external-link>
-      </h3>
-      <p>
-        {{
-          $t(
-            'テレワーク・時差出勤などスムーズビズの取組は、新型コロナウイルス感染症の対策としても効果的です。感染症対策として、東京2020大会時の交通混雑緩和に向けた取組の前倒しをお願いします。'
+            '新型コロナウイルス感染症に関する労働相談等について、特別労働相談窓口を設けました。'
           )
         }}
       </p>
