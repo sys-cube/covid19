de4bf7130b8de0cb744320502681f0c8b318f2bf
坂上善浩 <y-sakagami@sys-cube.co.jp>
2020-04-17 20:54:20 +0900

    aboutの不要項目を削除

--- pages/about.vue
+++ pages/about.vue
@@ -92,82 +92,6 @@
       </p>
     </static-card>
     <static-card>
-      <h3>{{ $t('Google Analyticsの利用について') }}</h3>
-      <p>
-        {{
-          $t(
-            '当サイトでは、サービス向上やサイトの改善のためにGoogle LLCの提供するアクセス分析のツールであるGoogle Analyticsを利用した計測を行っております。'
-          )
-        }}
-      </p>
-      <p>
-        {{
-          $t(
-            'Google Analyticsでは、当サイトが発行するクッキー (Cookie) 等を利用して、Webサイトの利用データ（アクセス状況、トラフィック、閲覧環境、IPアドレスなど）を収集しております。クッキーの利用に関してはGoogleのプライバシーポリシーと規約に基づいております。'
-          )
-        }}
-      </p>
-      <p>
-        {{
-          $t(
-            '取得したデータはWebサイト利用状況を分析しサービスの改善につなげるため、またはサイト運営者へのレポートを作成するため、その他のサービスの提供に関わる目的に限り、これを使用します。（サイト運営者へのレポートでは、クッキーはブラウザ単位で本サイトのユーザー数をカウントするため、IPアドレスはGoogle Analyticsの分析機能を通じてアクセス元の地域分布（国、州・都道府県、都市）を把握するために利用されています。）'
-          )
-        }}
-      </p>
-      <p>
-        {{
-          $t(
-            'Google Analyticsの利用規約及びプライバシーポリシーに関する説明については、Google Analyticsのサイトをご覧ください。'
-          )
-        }}
-      </p>
-      <ul>
-        <li>
-          <external-link
-            :url="
-              $t(
-                'https://marketingplatform.google.com/about/analytics/terms/jp/'
-              )
-            "
-            :icon-size="16"
-          >
-            {{ $t('Google Analytics利用規約') }}
-          </external-link>
-        </li>
-        <li>
-          <external-link
-            :url="$t('https://policies.google.com/privacy?hl=ja')"
-            :icon-size="16"
-          >
-            {{ $t('Googleのプライバシーポリシー') }}
-          </external-link>
-        </li>
-        <li>
-          <external-link
-            :url="
-              $t('https://support.google.com/analytics/answer/6004245?hl=ja')
-            "
-            :icon-size="16"
-          >
-            {{ $t('Google Analyticsに関する詳細情報') }}
-          </external-link>
-        </li>
-      </ul>
-      <i18n
-        tag="p"
-        path="Google Analyticsによる情報送信を回避する場合は、Google がサポートする{addon}をご利用ください。"
-      >
-        <template v-slot:addon>
-          <external-link
-            :url="$t('https://tools.google.com/dlpage/gaoptout?hl=ja')"
-            :icon-size="16"
-          >
-            {{ $t('測定を無効にするブラウザ アドオン') }}
-          </external-link>
-        </template>
-      </i18n>
-    </static-card>
-    <static-card>
       <h3>{{ $t('免責事項') }}</h3>
       <p>
         {{
@@ -190,22 +114,6 @@
           )
         }}
       </p>
-    </static-card>
-    <static-card>
-      <h3>{{ $t('データについて') }}</h3>
-      <i18n
-        tag="p"
-        path="本サイトで公表しているデータは、{catalogWebsite}より誰でも自由にダウンロードが可能です。（データは順次追加予定です）"
-      >
-        <template v-slot:catalogWebsite>
-          <external-link
-            url="https://portal.data.metro.tokyo.lg.jp/"
-            :icon-size="16"
-          >
-            {{ $t('東京都オープンデータカタログサイト') }}
-          </external-link>
-        </template>
-      </i18n>
     </static-card>
     <static-card>
       <h3>{{ $t('ソースコードについて') }}</h3>
