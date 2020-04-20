7224d74870865ddd45f628201b906613acd9ff2a
Takahiro Fukushima <t_fukushima@sys-cube.co.jp>
2020-04-20 19:36:41 +0900

    google analytics設定

--- pages/about.vue
+++ pages/about.vue
@@ -88,6 +88,82 @@
       </p>
     </static-card>
     <static-card>
+      <h3>{{ $t('Google Analyticsの利用について') }}</h3>
+      <p>
+        {{
+          $t(
+            '当サイトでは、サービス向上やサイトの改善のためにGoogle LLCの提供するアクセス分析のツールであるGoogle Analyticsを利用した計測を行っております。'
+          )
+        }}
+      </p>
+      <p>
+        {{
+          $t(
+            'Google Analyticsでは、当サイトが発行するクッキー (Cookie) 等を利用して、Webサイトの利用データ（アクセス状況、トラフィック、閲覧環境、IPアドレスなど）を収集しております。クッキーの利用に関してはGoogleのプライバシーポリシーと規約に基づいております。'
+          )
+        }}
+      </p>
+      <p>
+        {{
+          $t(
+            '取得したデータはWebサイト利用状況を分析しサービスの改善につなげるため、またはサイト運営者へのレポートを作成するため、その他のサービスの提供に関わる目的に限り、これを使用します。（サイト運営者へのレポートでは、クッキーはブラウザ単位で本サイトのユーザー数をカウントするため、IPアドレスはGoogle Analyticsの分析機能を通じてアクセス元の地域分布（国、州・都道府県、都市）を把握するために利用されています。）'
+          )
+        }}
+      </p>
+      <p>
+        {{
+          $t(
+            'Google Analyticsの利用規約及びプライバシーポリシーに関する説明については、Google Analyticsのサイトをご覧ください。'
+          )
+        }}
+      </p>
+      <ul>
+        <li>
+          <external-link
+            :url="
+              $t(
+                'https://marketingplatform.google.com/about/analytics/terms/jp/'
+              )
+            "
+            :icon-size="16"
+          >
+            {{ $t('Google Analytics利用規約') }}
+          </external-link>
+        </li>
+        <li>
+          <external-link
+            :url="$t('https://policies.google.com/privacy?hl=ja')"
+            :icon-size="16"
+          >
+            {{ $t('Googleのプライバシーポリシー') }}
+          </external-link>
+        </li>
+        <li>
+          <external-link
+            :url="
+              $t('https://support.google.com/analytics/answer/6004245?hl=ja')
+            "
+            :icon-size="16"
+          >
+            {{ $t('Google Analyticsに関する詳細情報') }}
+          </external-link>
+        </li>
+      </ul>
+      <i18n
+        tag="p"
+        path="Google Analyticsによる情報送信を回避する場合は、Google がサポートする{addon}をご利用ください。"
+      >
+        <template v-slot:addon>
+          <external-link
+            :url="$t('https://tools.google.com/dlpage/gaoptout?hl=ja')"
+            :icon-size="16"
+          >
+            {{ $t('測定を無効にするブラウザ アドオン') }}
+          </external-link>
+        </template>
+      </i18n>
+    </static-card>
+    <static-card>
       <h3>{{ $t('免責事項') }}</h3>
       <p>
         {{
