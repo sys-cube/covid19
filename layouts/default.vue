f94ff16a85e7f7ecb6457e0ad5990ecd70fe77db
坂上善浩 <y-sakagami@sys-cube.co.jp>
2020-04-24 09:41:22 +0900

    Revert "githubでビルドしてnetlifyにデプロイされるかテスト"
    This reverts commit 10198b1d9accab2155e08b3760e29346156e9a66.

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <v-app class="app">
     <v-overlay v-if="loading" color="#F8F9FA" opacity="1" z-index="9999">
       <div class="loader">
-        <img src="/logo.svg" alt="和歌山県githubビルドテスト" />
+        <img src="/logo.svg" alt="和歌山県" />
         <scale-loader color="#00A040" />
       </div>
     </v-overlay>
