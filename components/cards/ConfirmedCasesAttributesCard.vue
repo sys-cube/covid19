21c504d30ae74b61da013f3510d8d97dc32bd9be
辻本柾 <m_tsujimoto@sys-cube.co.jp>
2021-04-13 18:45:27 +0900

    * 修正

--- components/cards/ConfirmedCasesAttributesCard.vue
+++ components/cards/ConfirmedCasesAttributesCard.vue
@@ -46,8 +46,7 @@ export default {
 
     // 陽性患者の属性 ヘッダー翻訳
     for (const header of patientsTable.headers) {
-      header.text =
-        header.value === '退院' ? this.$t('退院※') : this.$t(header.value)
+      header.text = this.$t(header.value)
     }
     // 陽性患者の属性 中身の翻訳
     for (const row of patientsTable.datasets) {
